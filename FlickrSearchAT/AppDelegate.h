22940d6ba07a0eb10ecdb9939ce01ae4d116aa99
Suhail Rashid Bhat <suhailrashid@live.com>
2015-12-06 11:53:38 +0530

    Initial Commit

---
+++ FlickrSearchAT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlickrSearchAT
+//
+//  Created by Suhail Bhat on 06/12/15.
+//  Copyright © 2015 SRB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
